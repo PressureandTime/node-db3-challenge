66e8397e6fe63a71470dbf9b36e2c8828736dc3e
Petar Scepanovic <hiborija@gmail.com>
2019-07-17 15:35:09 +0200

    wrote helper for fetching all the steps associated with particular scheme and adding a new scheme

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,7 +4,33 @@ function find() {
   return db('schemes');
 }
 
+function findById(id) {
+  return db('schemes').where({ id });
+}
+
+function findSteps(schemeId) {
+  return db('steps as s')
+    .join('schemes as sch', 's.step_number', 'sch.id')
+    .select(
+      's.id',
+      's.instructions',
+      'sch.id as SchemeId',
+      'sch.scheme_name as scheme',
+    )
+    .where({ step_number: schemeId });
+}
+
+
+function add({ scheme_name }) {
+  return db('schemes').insert({ scheme_name });
+}
+
+
+
 
 module.exports = {
   find,
-}
+  findById,
+  findSteps,
+  add,
+};
